f35a8edb160d118b88cfc070d2d543973b046ac9
Mehboob-Alam1 <ma8122576@gmail.com>
2024-01-11 05:11:50 -0800

    audio player added

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,78 @@ void main() {
 class Xylophone extends StatelessWidget {
   const Xylophone({super.key});
 
+  void playSound(int i){
+    final player = AudioPlayer();
+    player.play(AssetSource('note$i.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-            child: OutlinedButton(
-          onPressed: () {
-            final player = AudioPlayer();
-             player.play(AssetSource('note1.wav'));
-          },
-          child: Text("Click me"),
-        )),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            TextButton(
+
+              onPressed: () {
+               playSound(1);
+              },
+              child: Text("Sound 1"),
+
+            ),
+            TextButton(
+
+              onPressed: () {
+               playSound(2);
+              },
+              child: Text("Sound 2"),
+
+            ),
+            TextButton(
+
+              onPressed: () {
+                playSound(3);
+              },
+              child: Text("Sound 3"),
+
+            ),
+            TextButton(
+
+              onPressed: () {
+               playSound(4);
+              },
+              child: Text("Sound 4"),
+
+            ),
+            TextButton(
+
+              onPressed: () {
+                playSound(5);
+              },
+              child: Text("Sound 5"),
+
+            ),
+            TextButton(
+
+              onPressed: () {
+                playSound(6);
+              },
+              child: Text("Sound 6"),
+
+            ),
+            TextButton(
+
+              onPressed: () {
+              playSound(7);
+              },
+              child: Text("Sound 7"),
+
+            ),
+
+          ],
+        ),
       ),
     );
   }
