3a9af9f701c5bf1bcb596442ea96a0526efd339c
Mehboob-Alam1 <ma8122576@gmail.com>
2024-01-13 03:38:09 -0800

    added the sound and made the app with expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-
-void main() {
-  runApp(Xylophone());
-}
+void main() => runApp(Xylophone());
 
 class Xylophone extends StatelessWidget {
   const Xylophone({super.key});
 
-  void playSound(int i){
+  void playSound(int i) {
     final player = AudioPlayer();
     player.play(AssetSource('note$i.wav'));
+  }
+
+  Expanded playBuildKey({Color? color, int? sound}) {
+    return Expanded(
+      child: TextButton(
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<OutlinedBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.zero)),
+          backgroundColor: MaterialStateProperty.all<Color>(
+              color!), // Set your desired background color
+        ),
+        onPressed: () {
+          playSound(sound!);
+        },
+        child: Text(
+            style: TextStyle(
+              color: Colors.white,
+            ),
+            ""),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
-          children: [
-            TextButton(
-
-              onPressed: () {
-               playSound(1);
-              },
-              child: Text("Sound 1"),
-
-            ),
-            TextButton(
-
-              onPressed: () {
-               playSound(2);
-              },
-              child: Text("Sound 2"),
-
-            ),
-            TextButton(
-
-              onPressed: () {
-                playSound(3);
-              },
-              child: Text("Sound 3"),
-
-            ),
-            TextButton(
-
-              onPressed: () {
-               playSound(4);
-              },
-              child: Text("Sound 4"),
-
-            ),
-            TextButton(
-
-              onPressed: () {
-                playSound(5);
-              },
-              child: Text("Sound 5"),
-
-            ),
-            TextButton(
-
-              onPressed: () {
-                playSound(6);
-              },
-              child: Text("Sound 6"),
-
-            ),
-            TextButton(
-
-              onPressed: () {
-              playSound(7);
-              },
-              child: Text("Sound 7"),
-
-            ),
-
-          ],
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              playBuildKey(color: Colors.black, sound: 1),
+              playBuildKey(color: Colors.red, sound: 2),
+              playBuildKey(color: Colors.blue, sound: 3),
+              playBuildKey(color: Colors.orange, sound: 4),
+              playBuildKey(color: Colors.purple, sound: 5),
+              playBuildKey(color: Colors.yellow, sound: 6),
+              playBuildKey(color: Colors.pinkAccent, sound: 7),
+            ],
+          ),
         ),
       ),
     );
